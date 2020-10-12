aab725828f5766727add90d65e290a982a2c8822
davidaskenazy <davidask.da@gmail.com>
2020-10-12 12:52:07 -0300

    primer push a rama dev

---
+++ lib/home.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:tik_tok_ui/widgets/video_description.dart';
+import 'package:tik_tok_ui/widgets/actions_toolbar.dart';
+import 'package:tik_tok_ui/widgets/bottom_toolbar.dart';
+
+class Home {}
